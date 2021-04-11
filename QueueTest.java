1f659e2f74cdf381fb16690825c24df9ce8f51e9
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-04-11 22:17:44 +0530

    ADDED : test to validate dequeue method

--- QueueTest.java
+++ QueueTest.java
@@ -18,4 +18,21 @@ public class QueueTest {
         Assertions.assertEquals(customQueue.showLatestNodeOfQueue(), firstNode);
     }
 
+    @Test
+
+    public void whenDequeued_TailShouldBeMovedBackOneNode() {
+        NodeClass<Integer> firstNode = new NodeClass<>(70);
+        NodeClass<Integer> secondNode = new NodeClass<>(30);
+        NodeClass<Integer> thirdNode = new NodeClass<>(56);
+
+        Queue customQueue = new Queue();
+
+        customQueue.enqueue(firstNode);
+        customQueue.enqueue(secondNode);
+        customQueue.enqueue(thirdNode);
+
+        customQueue.dequeue();
+
+        Assertions.assertEquals(customQueue.showFirstNodeOfQueue(), secondNode);
+    }
 }
